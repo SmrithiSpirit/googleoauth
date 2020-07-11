19d50ec4fe5d97297e9bf5c54e7371ffb86ef65f
SmrithiSpirit <45619581+SmrithiSpirit@users.noreply.github.com>
2020-07-12 01:25:13 +0530

    Update gauth.js

--- gauth.js
+++ gauth.js
@@ -1,4 +1,4 @@
 window.addEventListener('message', function(eventData) {
 	let event = JSON.parse(eventData.data);
-	console.log("I want to login" + event.data.code)
+	console.log("I want to login" + event)
 })
