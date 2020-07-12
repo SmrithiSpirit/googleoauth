cc6bf94c3308c89049dac6c33590e2737a907623
SmrithiSpirit <45619581+SmrithiSpirit@users.noreply.github.com>
2020-07-12 20:21:48 +0530

    Update gauth.js

--- gauth.js
+++ gauth.js
@@ -78,6 +78,6 @@ window.addEventListener('message', function(eventData) {
 	let event = JSON.parse(eventData.data);
 	console.log("I want to login")
 	if (event.data == "Login"){
-		//trySampleRequest();
+		trySampleRequest();
 	}
 })
